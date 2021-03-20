666ae82a53824fa5de599456b9408c4e2f9886ab
Abner Alves de Oliveira <abneralves10@hotmail.com>
2021-03-20 14:36:11 -0300

    Created frame of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'episodes', loadChildren: () => import('./components/pages/episodes/episodes.module').then(m => m.EpisodesModule) },
+  { path: 'about', loadChildren: () => import('./components/pages/about/about.module').then(m => m.AboutModule) },
+  { path: 'characters', loadChildren: () => import('./components/pages/characters/characters.module').then(m => m.CharactersModule) },
+  { path: 'notFound', loadChildren: () => import('./components/pages/not-found/not-found.module').then(m => m.NotFoundModule) }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/components/pages/characters/characters-details/characters-details.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharactersDetailsComponent } from './characters-details.component';
+
+
+
+@NgModule({
+  declarations: [CharactersDetailsComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharactersDetailsModule { }
